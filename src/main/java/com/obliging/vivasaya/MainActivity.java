cf6b4cf8cff86799a0b5c5f1feab6a850fa0e202
kiruthikraaj <44291333+kiruthikraaj@users.noreply.github.com>
2019-10-05 10:50:32 +0530

    Add files via upload

--- src/main/java/com/obliging/vivasaya/MainActivity.java
+++ src/main/java/com/obliging/vivasaya/MainActivity.java
@@ -1,33 +1,86 @@
 package com.obliging.vivasaya;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signout;
+    NavigationView navigationView;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActionBarDrawerToggle drawerToggle;
     FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signout = findViewById(R.id.signOut);
-        String uid = firebaseAuth.getCurrentUser().getUid();
+        final String uid = firebaseAuth.getCurrentUser().getUid();
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawerToggle  = new ActionBarDrawerToggle(this,drawer,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
-        signout.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Home()).commit();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                firebaseAuth.signOut();
-                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new Home();
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_share:
+                        fragment = new Share();
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_signout:
+                        firebaseAuth.signOut();
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+                return true;
             }
         });
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_share, R.id.nav_signout)
+                .setDrawerLayout(drawer)
+                .build();
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        return drawerToggle.onOptionsItemSelected(menuItem) || super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
     @Override
     public void onBackPressed() {
 
